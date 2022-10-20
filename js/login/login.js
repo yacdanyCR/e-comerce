9550d99c494eecd3e409324d8f4ffc29d7b50137
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-20 09:31:21 -0600

    redirection to page AddProduct.html

--- js/login/login.js
+++ js/login/login.js
@@ -1,7 +1,7 @@
 export const authLogin=({email,password})=>{
     const localUser=JSON.parse(localStorage.getItem("User"));
     if((localUser.email === email.value) && (localUser.password) === password.value){
-        window.location.href="../addProduct.html";
+        window.location.href="addProduct.html";
     }else{
         alert("Correo o contraseña incorrecta..!!!");
     }
