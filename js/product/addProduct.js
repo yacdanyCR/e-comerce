42caa57b6b77bd6ce7d23c3b3f74c180885d5cd7
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-20 15:52:27 -0600

    Adding id for each div

--- js/product/addProduct.js
+++ js/product/addProduct.js
@@ -4,21 +4,27 @@ export const addProduct = () => {
     const name = document.querySelector('[data-product="name"]');
     const price = document.querySelector('[data-product="price"]');
     const description = document.querySelector('[data-product="description"]');
+    const categorie = document.querySelector('[data-product="categorie"]');
+    const generateId = Math.random().toString(36).substr(2, 18);
 
     if (localStorage.getItem("Product") === null) {
         const objProduct = [{
+            id:generateId,
             name: name.value,
             price: price.value,
             description: description.value,
+            categorie:categorie.value,
             file: getFileConvertedtoBase64(reader.result)
         }];
         localStorage.setItem("Product", JSON.stringify(objProduct));
     } else {
         const getProducts = JSON.parse(localStorage.getItem("Product"));
         const newProduct = {
+            id:generateId,
             name: name.value,
             price: price.value,
             description: description.value,
+            categorie:categorie.value,
             file: getFileConvertedtoBase64(reader.result)
         }
         getProducts.push(newProduct);
--- js/product/showProduct.js
+++ js/product/showProduct.js
@@ -1,33 +1,42 @@
 import { getProducts } from "../../localStorage/localStorage.js";
 
 export const showProducts = () => {
-    if(getProducts() === null) return null;
+    if (getProducts() === null) return null;
     const product = getProducts();
-    const $section = document.querySelector('[data-section="starwars"]');
-    const fragment = document.createDocumentFragment();
 
     product.forEach(element => {
-        const $div = document.createElement("div");
-        const $img = document.createElement("img");
-        const $labelProductName = document.createElement("label");
-        const $labelProductPrice = document.createElement("label")
-        const $a = document.createElement("a");
-
-        $img.src = element.file;
-        $div.classList.add("galery__container");
-        $labelProductPrice.classList.add("product__price");
-        $a.classList.add("show__product--link");
-        $a.href="#";
-        $a.innerText="Ver Producto";
-
-        $div.appendChild($img);
-        $labelProductName.innerText = element.name;
-        $div.appendChild($labelProductName);
-        $labelProductPrice.innerText = element.price;
-        $div.appendChild($labelProductPrice);
-        $div.appendChild($a);
-
-        fragment.appendChild($div);
+        cardTemplate(element)
     });
+}
+
+const cardTemplate = (element) => {
+    let $section = document.querySelector(`[data-section="${element.categorie}"]`);
+    const $div = document.createElement("div");
+    const $img = document.createElement("img");
+    const $labelProductName = document.createElement("label");
+    const $labelProductPrice = document.createElement("label")
+    const $a = document.createElement("a");
+
+    $img.src = element.file;
+    $div.classList.add("galery__container");
+    $div.setAttribute('data-id',element.id);
+    $labelProductPrice.classList.add("product__price");
+    $a.classList.add("show__product--link");
+    $a.href = `showProduct.html`;
+    $a.innerText = "Ver Producto";
+
+    $div.appendChild($img);
+    $labelProductName.innerText = element.name;
+    $div.appendChild($labelProductName);
+    $labelProductPrice.innerText = element.price;
+    $div.appendChild($labelProductPrice);
+    $div.appendChild($a);
+
+    buildDivFragment($section, $div);
+}
+
+const buildDivFragment = ($section, $div) => {
+    const fragment = document.createDocumentFragment();
+    fragment.appendChild($div);
     $section.appendChild(fragment);
 }
